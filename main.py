129d15e017ba7ae4fc92bc13586f96851f1a814e
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-20 20:36:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import pytz
 #import flag
 #import emoji
 import numpy as np # biblioteca Python usada para trabalhar com arrays
-#import matplotlib.pyplot as plt # biblioteca para criar visualizações estáticas, animadas e interativas em Python
+import matplotlib.pyplot as plt # biblioteca para criar visualizações estáticas, animadas e interativas em Python
 #from os import write
 #from numpy.core.fromnumeric import size
 #import Controllers.clientecontroller as clientecontroller
@@ -635,9 +635,9 @@ def resumoApostas():
 
     espacos = ncoluna*[0.25]
     
-    #figura = plt.figure(figsize = (6,1))
+    figura = plt.figure(figsize = (6,1))
     
-    plt.figure(figsize = (6,1))
+    #plt.figure(figsize = (6,1))
     
     font = {'family':'serif', 'color':'black', 'weight':'normal', 'size':24}
     plt.title('Apostas Iniciais', fontdict = font)
