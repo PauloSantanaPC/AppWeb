4157f48e65b06d271ddd296b0ebf84386da31590
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-21 20:58:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1100,7 +1100,7 @@ def main():
                                     st.header(f'Resumo das apostas - {np.array(usuariosLista)[usuario][0]}')
                                     if usuariosLista[usuario][8] != '':
                                         opcoesBolao = ['Campeão do mundo','Vice de nada','cara que não sabe de futebol, mas não vai ser o pior do bolão','Pangaré do futebol']
-                                        st.subheader(f'Você vai ser o {opcoesBolao[int(usuariosLista[usuario][8])]} !')
+                                        st.subheader(f'Acha que vai ser o {opcoesBolao[int(usuariosLista[usuario][8])]} !')
                                     
                                     if np.array(usuariosLista)[usuario][9] != '':
                                         apostaCampeao = listaSelecoes()[int(np.array(usuariosLista)[usuario][9])]
