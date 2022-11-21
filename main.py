5da30464bf48f22c8857038b563c232a22261c28
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-21 11:38:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1092,14 +1092,17 @@ def main():
                            #st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
                         #st.subheader(usuariosLista[0])
-                        teste = [['Bolão',0],['Usuário 1',1],['Usuário 2',2]]
+                        #teste = [['Bolão',0],['Usuário 1',1],['Usuário 2',2]]
                         tabs = []
-                        for tab in range(len(teste)):
+                        #for tab in range(len(teste)):
+                        for tab in range(len(usuariosLista)):
                             tabs.append(teste[tab][0])
                         tabs = st.tabs(tabs)
-                        for usuario in range(len(teste)):
+                        #for usuario in range(len(teste)):
+                        for usuario in range(len(usuariosLista)):
                             with tabs[usuario]:
-                                st.header(f'Resumo das apostas: {teste[usuario][0]}')
+                                #st.header(f'Resumo das apostas: {teste[usuario][0]}')
+                                st.header(f'Resumo das apostas: {np.array(usuariosLista)[usuario][0]}')
                                 st.image("https://static.streamlit.io/examples/cat.jpg", width = 200)
                             
                     elif task1 == 'Links externos':
