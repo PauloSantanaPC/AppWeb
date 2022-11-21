c5b2ae1a4496b5d9491ff02e43447039a3f1c211
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-21 09:46:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1091,15 +1091,17 @@ def main():
                            #st.header("An owl")
                            #st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
-                        st.subheader(usuariosLista[0])
+                        #st.subheader(usuariosLista[0])
                         bolao, usuario1, usuario2 = st.tabs(["Bolão", "Dog", "Owl"])
-                        tabs = [bolao, usuario1, usuario2]
-                        ntabs = ["Bolão", "Usuário1", "Usuário2"]
+                        #tabs = [bolao, usuario1, usuario2]
+                        #ntabs = ["Bolão", "Usuário1", "Usuário2"]
+                        tabs = [["Bolão",bolao], ["Usuário2",usuario1], ["Usuário2",usuario2]]
                         for usuario in range(len(tabs)):
                         #for usuario in range(1, len(usuariosLista), 1):
-                            with tabs[usuario]:
+                            with tabs[usuario][:,0]:
                             #with usuariosLista[usuario][0]:
-                                st.header(f'Resumo das apostas: {ntabs[usuario]}')
+                                st.header(f'Resumo das apostas: {ntabs[usuario][:,0]}')
+                                #st.header(f'Resumo das apostas: {ntabs[usuario]}')
                                 #st.header(f'Resumo das apostas: {usuariosLista[usuario][0]}')
                                 st.image("https://static.streamlit.io/examples/cat.jpg", width = 200)
                             
