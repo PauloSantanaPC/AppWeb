6481f7576ae0b04801f694854893cfbd0b441272
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-21 15:32:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1091,8 +1091,8 @@ def main():
                                     st.header(f'Resumo das apostas do Bolão')
                                     st.image("https://static.streamlit.io/examples/owl.jpg", width = 200)
                                     #df = pd.DataFrame(np.random.randn(10, 5), columns=('col %d' % i for i in range(5)))
-                                    #df = pd.DataFrame(np.array([listaSelecoes()[int(np.array(usuariosLista)[usuario][9])], listaSelecoes()[int(np.array(usuariosLista)[usuario][10])], listaSelecoes()[int(np.array(usuariosLista)[usuario][11])]]),
-                                    df = pd.DataFrame(np.array([[1,2,3],[4,5,6]]),
+                                    df = pd.DataFrame(np.array([[listaSelecoes()[int(np.array(usuariosLista)[usuario][9])], listaSelecoes()[int(np.array(usuariosLista)[usuario][10])], listaSelecoes()[int(np.array(usuariosLista)[usuario][11])]]]),
+                                    #df = pd.DataFrame(np.array([[1,2,3],[4,5,6]]),
                                                       columns = ('Campeão','Vice-campeão','Terceiro colocado'))
                                     st.table(df)
                             else:
