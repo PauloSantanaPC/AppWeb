ab6d5c10f9de6957133f6bc0207985a7a255e8f2
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-23 15:04:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1098,11 +1098,12 @@ def main():
                                     usuariosLista[3][2] = -1
                                     classificacaoBolao = []
                                     for contadorUsuario in range(1, len(usuariosLista), 1):
-                                        classificacaoBolao.append(tuple([usuariosLista[contadorUsuario][0],usuariosLista[contadorUsuario][2],usuariosLista[contadorUsuario][3],usuariosLista[contadorUsuario][4],usuariosLista[contadorUsuario][5],usuariosLista[contadorUsuario][6],usuariosLista[contadorUsuario][7]]))
+                                        #classificacaoBolao.append(tuple([usuariosLista[contadorUsuario][0],usuariosLista[contadorUsuario][2],usuariosLista[contadorUsuario][3],usuariosLista[contadorUsuario][4],usuariosLista[contadorUsuario][5],usuariosLista[contadorUsuario][6],usuariosLista[contadorUsuario][7]]))
+                                        classificacaoBolao.append([usuariosLista[contadorUsuario][0],usuariosLista[contadorUsuario][2],usuariosLista[contadorUsuario][3],usuariosLista[contadorUsuario][4],usuariosLista[contadorUsuario][5],usuariosLista[contadorUsuario][6],usuariosLista[contadorUsuario][7]])
                                         
-                                    classificacaoBolao = np.array(classificacaoBolao, dtype = [('x', 'S10'), ('y1', int), ('y2', int), ('y3', int), ('y4', int), ('y5', int), ('y6', int)])
+                                    #classificacaoBolao = np.array(classificacaoBolao, dtype = [('x', 'S10'), ('y1', int), ('y2', int), ('y3', int), ('y4', int), ('y5', int), ('y6', int)])
                                     st.subheader(classificacaoBolao)
-                                    st.subheader(classificacaoBolao.sort(order='y1'))
+                                    #st.subheader(classificacaoBolao.sort(order='y1'))
                                     df0 = pd.DataFrame(np.array([[1,2,3,4,5,6],
                                                                  [4,5,6,7,8,9],
                                                                  [4,5,6,7,8,9],
