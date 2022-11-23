085b14bc4615a9cd416da03f881940b2993977bf
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-23 08:30:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1138,7 +1138,7 @@ def main():
                                                 apostasGrupos.append(listaApostasGruposUsuario)
 
                                     colunas = tuple(colunas)
-                                    st.subheader(apostasGrupos[0])
+                                    st.subheader(apostasGrupos[0][0])
                                     #df = pd.DataFrame(np.array([[1,2,3,4],[4,5,6,7]]),
                                     df = pd.DataFrame(np.array([opcoes,apostasCampeao,apostasViceCampeao,apostasTerceiroColocado,
                                                                 #apostasGrupos[0]
