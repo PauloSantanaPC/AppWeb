cd3353378d56eddae4e09bb608b6b10cbab26d8a
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-25 22:08:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1535,7 +1535,7 @@ def main():
                     elif task1 == 'Apostas fase de grupos':
                         st.title('Fase de Grupos')
                         classificacao = classificacaoInicial()
-                        usuario[2] = 1
+                        usuario[2] = 0
                         usuario[3] = 0
                         usuario[4] = 0
                         usuario[5] = 0
@@ -1779,6 +1779,7 @@ def main():
                                     for contadorUsuario in range(1, len(usuariosLista), 1):
                                         classificacaoBolao.append([usuariosLista[contadorUsuario][0],usuariosLista[contadorUsuario][2],usuariosLista[contadorUsuario][3],usuariosLista[contadorUsuario][4],usuariosLista[contadorUsuario][5],usuariosLista[contadorUsuario][6],usuariosLista[contadorUsuario][7]])
                                         dadosClassificacao.append(np.delete(np.array(classificacaoBolao[contadorUsuario-1]),0,0))
+                                        st.subheader(dadosClassificacao)
                                     df0 = pd.DataFrame(np.array(dadosClassificacao),
                                                        columns = ('Pontos','Cravadas','Acertos','Erros','Nadas','Não apostas'))
                                     df0.index = np.delete(np.array(usuariosLista)[:,0],0)
