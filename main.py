b3982201c8bcf86c35e13633c6adfc540ea29109
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-26 12:42:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1323,17 +1323,16 @@ def main():
                                             #pontuacaoJogo = 0
                                             for contadorUsuario in range(1, len(usuariosLista), 1):
                                                 #pontuacaoJogo = 0
-                                                st.subheader(type(usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo]))
-                                                st.subheader(f' gol mandante aposta {usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo]}')
-                                                st.subheader(type(usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo]))
-                                                st.subheader(f' gol visitante aposta {usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo]}')
-                                                st.subheader(type(usuario[28+2*6*nomeGrupo+2*nomeJogo]))
-                                                st.subheader(f' gol mandante jogo {usuario[28+2*6*nomeGrupo+2*nomeJogo]}')
-                                                st.subheader(type(usuario[29+2*6*nomeGrupo+2*nomeJogo]))
-                                                st.subheader(f' gol visitante jogo {usuario[29+2*6*nomeGrupo+2*nomeJogo]}')
+                                                #st.subheader(type(usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo]))
+                                                #st.subheader(f' gol mandante aposta {usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo]}')
+                                                #st.subheader(type(usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo]))
+                                                #st.subheader(f' gol visitante aposta {usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo]}')
+                                                #st.subheader(type(usuario[28+2*6*nomeGrupo+2*nomeJogo]))
+                                                #st.subheader(f' gol mandante jogo {usuario[28+2*6*nomeGrupo+2*nomeJogo]}')
+                                                #st.subheader(type(usuario[29+2*6*nomeGrupo+2*nomeJogo]))
+                                                #st.subheader(f' gol visitante jogo {usuario[29+2*6*nomeGrupo+2*nomeJogo]}')
                                                 #usuariosLista[contadorUsuario], pontuacao = resultadoApostadorFaseGrupos(usuariosLista[contadorUsuario],pontuacaoJogo,usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo],usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo],int(usuario[28+2*6*nomeGrupo+2*nomeJogo]),int(usuario[29+2*6*nomeGrupo+2*nomeJogo]))
                                                 usuariosLista[contadorUsuario], pontuacao = resultadoApostadorFaseGrupos(usuariosLista[contadorUsuario],pontuacaoJogo,usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo],usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo],usuario[28+2*6*nomeGrupo+2*nomeJogo],usuario[29+2*6*nomeGrupo+2*nomeJogo])
-                                                
                                                 st.subheader(f'A sua pontuação foi: {pontuacao} ponto(s)')
                                                 np.save(str(usuariosLista[contadorUsuario][0]),usuariosLista[contadorUsuario])
                                                 ##st.subheader(str(usuariosLista[contadorUsuario][0]))
