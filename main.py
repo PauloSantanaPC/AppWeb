77cf64484d38a60b818f842438cbda5ff4de98fd
PauloSantanaPC <118673327+PauloSantanaPC@users.noreply.github.com>
2022-11-26 12:21:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1325,7 +1325,9 @@ def main():
                                                 #pontuacaoJogo = 0
                                                 usuariosLista[contadorUsuario], pontuacao = resultadoApostadorFaseGrupos(usuariosLista[contadorUsuario],pontuacaoJogo,usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo],usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo],int(usuario[28+2*6*nomeGrupo+2*nomeJogo]),int(usuario[29+2*6*nomeGrupo+2*nomeJogo]))
                                                 #resultadoApostadorFaseGrupos(usuariosLista[contadorUsuario],pontuacaoJogo,usuariosLista[contadorUsuario][28+2*6*nomeGrupo+2*nomeJogo],usuariosLista[contadorUsuario][29+2*6*nomeGrupo+2*nomeJogo],int(usuario[28+2*6*nomeGrupo+2*nomeJogo]),int(usuario[29+2*6*nomeGrupo+2*nomeJogo]))
+                                                st.subheader(usuariosLista[contadorUsuario][0])
                                                 st.subheader(f'A sua pontuação foi: {pontuacao} ponto(s)')
+                                                st.subheader(usuariosLista[contadorUsuario][3])
                                                 np.save(str(usuariosLista[contadorUsuario][0]),usuariosLista[contadorUsuario])
                                                 ##st.subheader(str(usuariosLista[contadorUsuario][0]))
                                                 ##np.save(str(username),usuario)
